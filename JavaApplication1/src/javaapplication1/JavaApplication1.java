9186e31eb653c2821b2154317ce6d4c87bc90bcd
juanestabanramirez <juanestebanr77@gmail.com>
2018-08-03 11:30:09 -0500

    segundo cambie ejercicio nombres

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args tassdfvdhe command line arguments
+     */Sergio Suarique 20161020526
+     */juan esteban ramirez 20171025092
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
